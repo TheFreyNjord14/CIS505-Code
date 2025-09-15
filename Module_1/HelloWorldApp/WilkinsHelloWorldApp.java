303812d29a4d9545d265271da0e9835e633e1537
Sean Wilkins <spwilkins@my365.bellevue.edu>
2025-09-14 23:02:39 -0500

    Added printf.

---
+++ Module_1/HelloWorldApp/WilkinsHelloWorldApp.java
@@ -0,0 +1,12 @@
+package Module_1.HelloWorldApp;
+
+public class WilkinsHelloWorldApp
+{
+    public static void main (String[] args)
+    {
+        String first = "Seannace";
+        String last = "Wilkins";
+
+        System.out.printf("Hello World from %s %s.", first, last);
+    }
+}
